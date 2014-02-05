c05f7402a94d1c7109bf4f340fc7a084c6a0f47c
John Tibbetts <john.tibbetts@kinexis.com>
2014-02-05 05:21:50 -0800

    updates

--- lti2_tc/app/models/lti2_tc/link.rb
+++ lti2_tc/app/models/lti2_tc/link.rb
@@ -11,12 +11,14 @@ module Lti2Tc
 
     attr_accessible :enrollment_id, :resource_link_label, :tool_id, :link_parameters, :grade_item_id
 
-    def lti2_launch(user, return_url)
-      Lti2Tc::Lti2Launch.launch(user, self.course, self.resource, self.link, return_url)
+    def lti_launch(user, return_url)
+      enrollment = Enrollment.where(:admin_user_id => @user.id, :course_id => @course.id).first
+      Lti2Tc::Lti2Launch.new(user, self, self.resource, self.course, enrollment, return_url).launch()
     end
 
     def is_ready_for_use
       self.is_enabled and self.resource.tool.is_enabled
     end
+
   end
 end
--- lti2_tc/app/services/lti2_tc/lti2_launch.rb
+++ lti2_tc/app/services/lti2_tc/lti2_launch.rb
@@ -1,44 +1,25 @@
 module Lti2Tc
   class Lti2Launch
 
-    def self.launch user, course, resource, link, return_url
-
-      def tool_consumer_profile_resolver(fieldname)
-        if fieldname == "url"
-          @tool_proxy.first_at('tool_consumer_profile')
-        end
-      end
-
-      def tool_proxy_resolver(fieldname)
-        if fieldname == 'custom.url'
-          tool_guid = resource.tool.key
-          ToolSetting.create_uri(tool_guid, 'Tool')
-        end
-      end
-
-      def tool_proxy_binding_resolver(fieldname)
-        if fieldname == 'custom.url'
-          tool_guid = resource.tool.key
-          ToolSetting.create_uri(tool_guid, 'Context', course_id)
-        end
-      end
-
-      def lti_link_resolver(fieldname)
-        if fieldname == 'custom.url'
-          tool_guid = resource.tool.key
-          ToolSetting.create_uri(tool_guid, 'Ltilink', link.id)
-        end
-      end
-
+    def initialize(user, link, resource, course, enrollment, return_url, &substitute_final)
+      @user = user
+      @link = link
+      @resource = resource
+      @course = course
+      @enrollment = enrollment
+      @return_url = return_url
       @tool_proxy = resource.tool.get_tool_proxy
+      @substitute_final = substitute_final
+    end
 
-      tool = resource.tool
+    def launch()
+      tool = @resource.tool
       tool_name = @tool_proxy.first_at('tool_profile.product_instance.product_info.product_name.default_value')
       raise "Tool #{tool_name} is currently disabled" unless tool.is_enabled
 
       base_url = @tool_proxy.select('tool_profile.base_url_choice',
                                     "selector.applies_to", "MessageHandler", 'default_base_url')
-      resource_type = resource.resource_type
+      resource_type = @resource.resource_type
       resource_handler_node = @tool_proxy.search("tool_profile.resource_handler", {'resource_type' => {'code' => resource_type}}, "@")
       resource_handler = JsonWrapper.new resource_handler_node
       message = resource_handler.search("@..message", {'message_type' => 'basic-lti-launch-request'}, '@')
@@ -63,11 +44,10 @@ module Lti2Tc
       tp_parameters = message['parameter']
       service_endpoint = base_url + final_path
 
-      enrollment = Enrollment.where(:admin_user_id => user.id, :course_id => course.id).first
-      if enrollment
-        role = enrollment.role
+      if @enrollment
+        role = @enrollment.role
       else
-        role = user.role
+        role = @user.role
       end
 
       tool_consumer_registry = Rails.application.config.tool_consumer_registry
@@ -75,37 +55,37 @@ module Lti2Tc
       parameters = {
           'lti_version' => @tool_proxy.first_at('lti_version'),
           'lti_message_type' => 'basic-lti-launch-request',
-          'resource_link_id' => link.id.to_s,
-          'user_id' => user.id.to_s,
+          'resource_link_id' => @link.id.to_s,
+          'user_id' => @user.id.to_s,
           'roles' => role,
-          'launch_presentation_return_url' => "#{tool_consumer_registry.tc_deployment_url}#{return_url}",
+          'launch_presentation_return_url' => "#{tool_consumer_registry.tc_deployment_url}#{@return_url}",
           'launch_presentation_document_target' => 'window',
 
           # optional
-          'context_id' => course.id.to_s,
+          'context_id' => @course.id.to_s,
           'launch_presentation_locale' => 'en_US',
 
           # for conformance
-          'context_label' => course.course_label,
-          'context_title' => course.course_title,
-          'lis_course_section_sourcedid' => course.id.to_s,
+          'context_label' => @course.course_label,
+          'context_title' => @course.course_title,
+          'lis_course_section_sourcedid' => @course.id.to_s,
           'tool_consumer_info_product_family_code' => "#{tcp_wrapper.first_at("product_instance.product_info.product_family.code")}",
           'tool_consumer_info_version' => "#{tcp_wrapper.first_at("product_instance.product_info.product_version")}",
       }
 
-      if link.is_name_permission
-        parameters['lis_person_sourcedid'] = user.id.to_s
-        parameters['lis_person_name_given'] = user.first_name
-        parameters['lis_person_name_family'] = user.last_name
-        parameters['lis_person_name_full'] = "#{user.first_name} #{user.last_name}"
+      if @link.is_name_permission
+        parameters['lis_person_sourcedid'] = @user.id.to_s
+        parameters['lis_person_name_given'] = @user.first_name
+        parameters['lis_person_name_family'] = @user.last_name
+        parameters['lis_person_name_full'] = "#{@user.first_name} #{@user.last_name}"
       end
 
-      if link.is_email_permission
-        parameters['lis_person_contact_email_primary'] = user.email
+      if @link.is_email_permission
+        parameters['lis_person_contact_email_primary'] = @user.email
       end
 
       if role =~ /Mentor$/
-        parameters['role_scope_mentor'] = url_encode_users(user.mentor)
+        parameters['role_scope_mentor'] = url_encode_users(@user.mentor)
       end
 
       # add parameters from ToolProxy
@@ -122,7 +102,7 @@ module Lti2Tc
       end
 
       # add parameters from Link
-      link_parameter_json = link_parameters
+      link_parameter_json = @link.link_parameters
       if link_parameter_json
         link_parameters = JSON.parse link_parameter_json
         parameters.merge! link_parameters
@@ -132,17 +112,17 @@ module Lti2Tc
       settings = Lti2Tc::ToolSetting.where(:tool_id => tool.id, :scopeable_type => 'Tool')
       settings.each {|setting| parameters[setting.name] = setting.value}
 
-      settings = Lti2Tc::ToolSetting.where(:tool_id => tool.id, :scopeable_id => course.id, :scopeable_type => 'Context')
+      settings = Lti2Tc::ToolSetting.where(:tool_id => tool.id, :scopeable_id => @course.id, :scopeable_type => 'Context')
       settings.each {|setting| parameters[setting.name] = setting.value}
 
-      settings = Lti2Tc::ToolSetting.where(:tool_id => tool.id, :scopeable_id => link.id, :scopeable_type => 'Ltilink')
+      settings = Lti2Tc::ToolSetting.where(:tool_id => tool.id, :scopeable_id => @link.id, :scopeable_type => 'Ltilink')
       settings.each {|setting| parameters[setting.name] = setting.value}
 
       # auto-create result (if required) and add to reference to parameters
       capabilities = resource_handler.first_at('message[0].enabled_capability')
       if capabilities and capabilities.include? "Result.autocreate"
-        if link.grade_item_id
-          grade_result = GradeResult.where(:link_id => link.id, :admin_user_id => user.id).first
+        if @link.grade_item_id
+          grade_result = GradeResult.where(:link_id => @link.id, :admin_user_id => @user.id).first
           # TEST ONLY
           # grade_result.delete if grade_result
           # grade_result = nil
@@ -150,7 +130,7 @@ module Lti2Tc
 
           unless grade_result
             grade_result = GradeResult.new
-            grade_result.link_id = link.id
+            grade_result.link_id = @link.id
             grade_result.admin_user_id = user.id
             grade_result.save
           else
@@ -164,10 +144,11 @@ module Lti2Tc
 
       # perform variable substitution on all parameters
       resolver = Lti2Commons::SubstitutionSupport::Resolver.new
-      resolver.add_resolver("User", user.method(:user_resolver))
-      resolver.add_resolver("Person", user.method(:person_resolver))
-      course = Course.find(parameters['context_id'])
-      resolver.add_resolver("CourseOffering", course.method(:course_resolver))
+      resolver.add_resolver("User", @user.method(:user_resolver))
+      resolver.add_resolver("Person", @user.method(:person_resolver))
+      # now already resolved
+      #@course = Course.find(parameters['context_id'])
+      resolver.add_resolver("CourseOffering", @course.method(:course_resolver))
       resolver.add_resolver("Result", grade_result.method(:grade_result_resolver)) if grade_result
 
       # this one is a bit deceptive.  a TCP is usually associated with a @tool_proxy
@@ -195,8 +176,10 @@ module Lti2Tc
 
       }
 
-      key = resource.tool.key
-      secret = resource.tool.secret
+      @substitute_final.call(final_parameters) unless @substitute_final.nil?
+
+      key = @resource.tool.key
+      secret = @resource.tool.secret
 
       signed_request = Signer::create_signed_request service_endpoint, 'post', key, secret, final_parameters
       puts "TC Signed Request: #{signed_request.signature_base_string}"
@@ -212,7 +195,7 @@ module Lti2Tc
 
 
 
-    def self.deprecated_lti_parameters
+    def deprecated_lti_parameters
       ["context_title", "context_label", "resource_link_title", "resource_link_description",
        "lis_person_name_given", "lis_person_name_family", "lis_person_name_full", "lis_person_contact_email_primary",
        "user_image", "lis_person_sourcedid", "lis_course_offering_sourcedid", "lis_course_section_sourcedid",
@@ -220,7 +203,7 @@ module Lti2Tc
        "tool_consumer_instance_name", "tool_consumer_instance_description", "tool_consumer_instance_url", "tool_consumer_instance_contact_email"]
     end
 
-    def self.known_lti2_parameters
+    def known_lti2_parameters
       ["lti_message_type", "lti_version", "user_id", "roles", "resource_link_id",
        "context_id", "context_type",
        "launch_presentation_locale", "launch_presentation_document_target", "launch_presentation_css_url",
@@ -228,7 +211,35 @@ module Lti2Tc
        "role_scope_mentor"]
     end
 
-    def self.slugify name
+
+    def tool_consumer_profile_resolver(fieldname)
+      if fieldname == "url"
+        @tool_proxy.first_at('tool_consumer_profile')
+      end
+    end
+
+    def tool_proxy_resolver(fieldname)
+      if fieldname == 'custom.url'
+        tool_guid = @resource.tool.key
+        ToolSetting.create_uri(tool_guid, 'Tool')
+      end
+    end
+
+    def tool_proxy_binding_resolver(fieldname)
+      if fieldname == 'custom.url'
+        tool_guid = @resource.tool.key
+        ToolSetting.create_uri(tool_guid, 'Context', @course.id)
+      end
+    end
+
+    def lti_link_resolver(fieldname)
+      if fieldname == 'custom.url'
+        tool_guid = @resource.tool.key
+        ToolSetting.create_uri(tool_guid, 'Ltilink', @link.id)
+      end
+    end
+
+    def slugify name
       result = ""
       name.each_char { |c|
         result << (c =~ /\w/ ? c.downcase : '_')
@@ -236,7 +247,7 @@ module Lti2Tc
       result
     end
 
-    def self.url_encode_users(user_list_str)
+    def url_encode_users(user_list_str)
       user_list = []
       unless user_list_str.nil?
         users = user_list_str.split(',')
