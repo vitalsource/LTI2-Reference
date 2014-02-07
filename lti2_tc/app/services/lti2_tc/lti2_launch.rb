7176862cd9c686c9b1182db0941ac97890b5ce16
John Tibbetts <john.tibbetts@kinexis.com>
2014-02-07 14:07:07 -0800

    update

--- lti2_tc/app/services/lti2_tc/lti2_launch.rb
+++ lti2_tc/app/services/lti2_tc/lti2_launch.rb
@@ -186,7 +186,7 @@ module Lti2Tc
       puts "before"
       puts Rails.application.config.wire_log.inspect
       puts "after"
-      body = MessageSupport::create_lti_message_body(service_endpoint, final_parameters, Rails.application.config.wire_log, "Lti Launch", true)
+      body = MessageSupport::create_lti_message_body(service_endpoint, subst_final_map, Rails.application.config.wire_log, "Lti Launch", true)
       puts body
       body
 
