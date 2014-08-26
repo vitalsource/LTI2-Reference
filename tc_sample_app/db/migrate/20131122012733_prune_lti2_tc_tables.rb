8f6cf9379819239bfd43a9d068efcdd7ca17e435
jrissler <risslerjames@gmail.com>
2014-08-26 12:54:26 -0400

    Clean up TC sample migrations.

--- tc_sample_app/db/migrate/20131122012733_prune_lti2_tc_tables.rb
+++
@@ -1,9 +0,0 @@
-class PruneLti2TcTables < ActiveRecord::Migration
-  def change
-    drop_table :deployment_requests
-    drop_table :registries
-    drop_table :tool_consumer_profiles
-    drop_table :tool_settings
-    drop_table :tools
-  end
-end
--- tc_sample_app/db/migrate/20131203032004_remove_extras.rb
+++
@@ -1,9 +0,0 @@
-class RemoveExtras < ActiveRecord::Migration
-  def up
-    drop_table :resources
-    drop_table :links
-  end
-
-  def down
-  end
-end
