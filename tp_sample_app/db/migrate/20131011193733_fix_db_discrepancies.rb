81452708fbf74f48e76ce600dd8d2e1fc044c119
jrissler <risslerjames@gmail.com>
2014-10-20 09:50:27 -0400

    Move engine migration into sample app + update existing migrations.

--- tp_sample_app/db/migrate/20131011193733_fix_db_discrepancies.rb
+++ tp_sample_app/db/migrate/20131011193733_fix_db_discrepancies.rb
@@ -1,6 +1,5 @@
 class FixDbDiscrepancies < ActiveRecord::Migration
   def change
     add_column :deployment_proposals, :tenant_key, :string, :after => :id
-    add_column :tenants, :tenant_key, :string, :after => :id
   end
 end
