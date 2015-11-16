813ce84fa5579cec2ab0077013f9e3fd2b0a20a5
Paul Gray <pfbgray@gmail.com>
2015-11-15 19:52:49 -0500

    Removing tool_proxy_guid assignment on deployment_request object during lti tool registration

--- lti2_tc/app/models/lti2_tc/tool_registration.rb
+++ lti2_tc/app/models/lti2_tc/tool_registration.rb
@@ -24,7 +24,6 @@ module Lti2Tc
       # Only comment out to test old-style ('late') tool_proxy_guid creation
       tool_proxy_guid = UUID.generate
 
-      deployment_request.tool_proxy_guid = tool_proxy_guid
       deployment_request.status = 'submitted'
       deployment_request.save
 
